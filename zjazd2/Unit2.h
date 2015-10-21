16e63e436ef9e85da3679c11eb5dd590ddfbbbdc
Karol Cyprowski <karolcyprowski@gmail.com>
2015-10-21 09:50:23 +0200

    Create Unit2.h

---
+++ zjazd2/Unit2.h
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit2H
+#define Unit2H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm2 : public TForm
+{
+__published:	// IDE-managed Components
+	TEdit *Edit1;
+	TEdit *Edit2;
+	TLabel *OPCJE;
+	TLabel *Label1;
+	TLabel *Label2;
+	TButton *Button1;
+	TComboBox *ComboBox1;
+	TLabel *Label3;
+	void __fastcall Button1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm2(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm2 *Form2;
+//---------------------------------------------------------------------------
+#endif
